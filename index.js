4ba7448adf36af5d7b10e1f541b7addacd6f66c3
new <new@Pranavs-Mac-mini.local>
2024-07-05 03:15:17 +0530

    commi

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const token = 'grtprsn_bot'; // Replace with your Telegram Bot token
+const token = '7261460030:AAHhZ-j1KXD1d9DWspXkTPTFhhGbxST5FU4'; // Replace with your Telegram Bot token
 
 // Serve static files (e.g., index.html)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,13 +17,16 @@ bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const username=msg.chat.username
 
-  bot.sendMessage(`Hello ${username}` );
+  console.log(username)
+
+  bot.sendMessage(chatId,`Hare Krishna ${user} Bro, its bro`);
 });
 
 // Handle request to fetch user data
-app.get('/user', (req, res) => {
-  const user = req.query.user; // Assuming you handle user data retrieval
-  res.json({ first_name: user ? user.first_name : null });
+app.get('/', (req, res) => {
+    const username=msg.chat.username
+
+    res.send(`Hello ${username}`)
 });
 
 // Start the server
